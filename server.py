c64e83367554e1003aa5cce7417ae338df15db4a
Runxi Yu <a@andrewyu.org>
2023-10-29 14:56:31 +0800

    Reformat the code a little

--- server.py
+++ server.py
@@ -481,6 +481,7 @@ def login() -> Union[Response, werkzeugResponse, str]:
     response.set_cookie("session-id", session_id)
     return response
 
+
 if __name__ == "__main__":
     try:
         app.run(port=48139, debug=True)
